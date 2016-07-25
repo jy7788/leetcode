0126b391ed6328c8a3e1f46793832f16423e6708
jy7788 <1151871470@qq.com>
2016-07-25 19:49:02 +0800

    add 292

---
+++ src/292NimGame.py
@@ -0,0 +1,12 @@
+class Solution(object):
+	def canWinNim(self, n):
+		"""
+		:type n: int
+		:rtype: bool
+		"""
+		if n%4 !=0:
+			return True
+		else:
+			return False
+a = Solution()
+print a.canWinNim(4)
